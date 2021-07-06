97ae765d6bbc3915e4aa10975a14e7f2bad99c1d
Roy <rolexroney@gmail.com>
2021-07-06 20:58:21 +0300

    created initial landing page for farmshare

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render("index");
+}); 
+
+app.listen(8080);
